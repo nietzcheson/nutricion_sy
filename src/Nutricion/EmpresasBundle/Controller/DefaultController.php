3c35de55b0c8a900d46619568784243957d82997
Cristian Angulo Nova <cristianangulonova@gmail.com>
2014-09-10 06:22:47 -0500

    Creación del módulo Empresas e inicio del módulo de perfiles

--- src/Nutricion/EmpresasBundle/Controller/DefaultController.php
+++ src/Nutricion/EmpresasBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace Nutricion\EmpresasBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Nutricion\EmpresasBundle\Entity;
 use Symfony\Component\HttpFoundation\Request;
-
+use Doctrine\ORM\EntityRepository;
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -172,44 +172,74 @@ class DefaultController extends Controller
         ));
     }
 
-    public function productoAction($id)
+    public function perfilesAction()
     {
-      $producto = $this->getDoctrine()
-      ->getRepository('NutricionEmpresasBundle:Producto')
-      ->find($id);
-
-      if(!$producto){
-        throw $this->createNotFoundException("No se encuentra el producto ".$id);
-      }
-
-
-      return new Response("Si existe el producto ".$producto->getNombre());
+        return $this->render("NutricionEmpresasBundle:Default:perfiles.html.twig",
+          array(
+            "titulo"=>"Perfiles",
+            "btn_header" => array(
+              "title" => "Crear perfil",
+              "href" => $this->generateUrl("nutricion_crear_perfil")
+            ),
+          ));
     }
 
-    public function productosAction()
+    public function crearPerfilAction()
     {
-      $productos = $this->getDoctrine()
-      ->getRepository("NutricionEmpresasBundle:Producto")
-      ->findAll();
+      $perfil = new Entity\Perfiles();
+      $perfil->setIdTipoperfil("");
 
-      if(!$productos){
-        throw $this->createNotFoundException("No hay productos");
-      }
+      $form = $this->createFormBuilder($perfil)
+      ->add("id_tipoperfil","entity",array(
+        'class' => 'NutricionEmpresasBundle:TiposPerfil',
+        'query_builder' => function(EntityRepository $er){
+            return $er->createQueryBuilder('p')->orderBy('p.tipo','ASC');
+          },
+        'property' => 'tipo',
+        'empty_value' => 'Seleccione el tipo de perfil',
+        'label' => "Tipo de perfil"
+      ))
+      ->add('id_empresa','entity',array(
+        'class' => 'NutricionEmpresasBundle:Empresas',
+        'query_builder' => function(EntityRepository $er){
+          return $er->createQueryBuilder('e')->orderBy('e.id','ASC');
+          },
+        'property' => 'empresa',
+        'empty_value' => 'Seleccione la empresa',
+        'label' => 'Empresa',
+      ))
+      ->add('nombres','text')
+      ->add('apellidos','text')
+      ->add('direccion','text')
+      ->add('email','text')
+      ->add('frecuencia_email','entity', array(
+        'class' => 'NutricionEmpresasBundle:Frecuencias',
+        'query_builder' => function(EntityRepository $er){
+            return $er->createQueryBuilder('f')->orderBy('f.id','ASC');
+          },
+        'property' => 'frecuencia',
+        'empty_value' => 'Seleccione el tipo de frecuencia',
+        'label' => '¿Cada cuánto revisa su correo electrónico?',
+      ))
+      ->add('telefono_trabajo','text',array('label'=>'Teléfono del trabajo'))
+      ->add('telefono_casa','text',array('label'=>'Teléfono de la casa'))
+      ->add('telefono_celular','text')
+      ->add('edad','text')
+      ->add('estatura_metros','text')
+      ->add('estatura_centimetros','text')
+      ->add('fecha_nacimiento','text')
+      ->add('lugar_nacimiento','text')
+      ->getForm();
 
-      return $this->render("NutricionEmpresasBundle:Default:index.html.twig",array("productos"=>$productos));
-
+      return $this->render("NutricionEmpresasBundle:Default:crear-perfil.html.twig",
+        array(
+          "titulo"=>"Crear perfil",
+          "btn_header" => array(
+            "title" => "Regresar",
+            "href" => $this->generateUrl("nutricion_perfiles")
+          ),
+          "form"=>$form->createView(),
+        ));
     }
 
-    public function crearAction()
-    {
-      $empresa = new Entity\empresas();
-      $empresa->setEmpresa("Empresa3");
-      $empresa->setEmail("empresa3@empresa.com");
-
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($empresa);
-
-      echo "Empresa creada ".$empresa->getId();
-      exit();
-    }
 }
